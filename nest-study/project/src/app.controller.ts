901ca2fb20496d87ac36136a65a6a38bb5decabe
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-08-22 23:38:32 +0900

    Learn about controller

--- nest-study/project/src/app.controller.ts
+++ nest-study/project/src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Req } from '@nestjs/common';
+import { Request } from 'express';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
+  // 의존성 주입: class 안에서 service를 사용할 수 있게 해준다
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
+  @Get('hello/:id/:name')
+  getHello(
+    @Req() req: Request,
+    @Body() Body,
+    @Param() param: { id: string; name: string },
+  ): string {
+    console.log(param);
     return this.appService.getHello();
   }
 }
