f9cf89d87f3924abb871985a36aed08411a3aff3
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-08-23 14:24:12 +0900

    Learn about exception filter

--- nest-study/project/src/cats/cats.controller.ts
+++ nest-study/project/src/cats/cats.controller.ts
@@ -1,13 +1,26 @@
+import { HttpExceptionFilter } from './../http-exception.filter';
 import { CatsService } from './cats.service';
-import { Controller, Delete, Get, Patch, Post, Put } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  HttpException,
+  Patch,
+  Post,
+  Put,
+  UseFilters,
+} from '@nestjs/common';
 
 @Controller('cats')
+@UseFilters(HttpExceptionFilter)
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
 
   // cats/
   @Get()
+  // @UseFilters(HttpExceptionFilter)
   getAllCat() {
+    throw new HttpException('api is broken', 401);
     return 'all cat';
   }
 
--- nest-study/project/src/logger/logger.middleware.ts
+++ nest-study/project/src/logger/logger.middleware.ts
@@ -5,7 +5,6 @@ import { NextFunction, Request, Response } from 'express';
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   private logger = new Logger('HTTP');
-
   use(req: Request, res: Response, next: NextFunction) {
     res.on('finish', () => {
       this.logger.log(
