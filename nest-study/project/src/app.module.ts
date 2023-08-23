672324ba905bd6b216f999f5476097966d5aa1be
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-08-23 17:18:36 +0900

    Feat: Add cat schema and setting mongodb

--- nest-study/project/src/app.module.ts
+++ nest-study/project/src/app.module.ts
@@ -1,18 +1,28 @@
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 import { LoggerMiddleware } from './common/middlewares/logger.middleware';
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
+import * as mongoose from 'mongoose';
 
 @Module({
-  imports: [CatsModule],
+  imports: [
+    CatsModule,
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 
 // 미들웨어 적용
 export class AppModule implements NestModule {
+  private readonly isDev: boolean = process.env.MODE === 'dev' ? true : false;
   configure(consumer: MiddlewareConsumer) {
     consumer.apply(LoggerMiddleware).forRoutes('*');
+    // mongoose query로깅
+    mongoose.set('debug', this.isDev);
   }
 }
--- nest-study/project/src/cats/cats.controller.ts
+++ nest-study/project/src/cats/cats.controller.ts
@@ -1,16 +1,10 @@
 import { SuccessInterceptor } from './../common/interceptors/success.interceptor';
-import { PositiveIntPipe } from './../common/pipes/positiveint.pipe';
 import { HttpExceptionFilter } from '../common/exceptions/http-exception.filter';
 import { CatsService } from './cats.service';
 import {
   Controller,
-  Delete,
   Get,
-  Param,
-  ParseIntPipe,
-  Patch,
   Post,
-  Put,
   UseFilters,
   UseInterceptors,
 } from '@nestjs/common';
@@ -21,41 +15,28 @@ import {
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
 
-  // cats/
   @Get()
-  // @UseFilters(HttpExceptionFilter)
-  getAllCat() {
-    return 'all cat';
-  }
-
-  // cats/:id
-  @Get(':id')
-  // pipe를 사용하여 validation error까지 출력 가능
-  // parameter 데이터가 들어오면 유효성검사 실행
-  // pipe에 넣어 변환, 실패하였을 경우 exception으로 빠지게 됨.
-  getOneCat(@Param('id', ParseIntPipe, PositiveIntPipe) param: number) {
-    console.log(param);
-    console.log(typeof param);
-    return 'one cat';
+  getCurrentCat() {
+    return 'current cat';
   }
 
   @Post()
-  createCat() {
-    return 'create cat';
+  async signUp() {
+    return 'sign up';
   }
 
-  @Put(':id')
-  updateCat() {
-    return 'update cat';
+  @Post('login')
+  logIn() {
+    return 'login';
   }
 
-  @Patch(':id')
-  updatePartialCat() {
-    return 'update';
+  @Post('logout')
+  logOut() {
+    return 'logout';
   }
 
-  @Delete(':id')
-  deleteCat() {
-    return 'delete cat';
+  @Post('upload/cats')
+  uploadCatImg() {
+    return 'uploadImg';
   }
 }
