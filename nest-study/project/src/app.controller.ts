6dd8db04fe5f966e6e61c0a80192dafe5b6c3249
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-08-23 13:35:36 +0900

    About module and encapsulation

--- nest-study/project/src/app.controller.ts
+++ nest-study/project/src/app.controller.ts
@@ -1,3 +1,4 @@
+import { CatsService } from './cats/cats.service';
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 // AppService: 공급자 => app.module에서 사업자등록을 해야함.
@@ -5,7 +6,11 @@ import { AppService } from './app.service';
 // AppController: 소비자
 export class AppController {
   // appService: 제품
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    // cats.module에서 exports를 해줘야만 사용 가능 ( 캡슐화 )
+    private readonly catsService: CatsService,
+  ) {}
 
   @Get()
   getHello(): string {
--- nest-study/project/src/cats/cats.module.ts
+++ nest-study/project/src/cats/cats.module.ts
@@ -5,5 +5,7 @@ import { CatsService } from './cats.service';
 @Module({
   controllers: [CatsController],
   providers: [CatsService],
+  // 다른 모듈에서 접근할 수 있도록 열어준다. (은닉화 된 것을 public으로 바꿔준 상태)
+  exports: [CatsService],
 })
 export class CatsModule {}
