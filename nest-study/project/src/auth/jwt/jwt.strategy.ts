0062a1e2082ce34c1524262569adace858b4a572
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-08-24 15:05:05 +0900

    Refactor: Move jwtSecret to .env for better security

--- nest-study/project/src/auth/jwt/jwt.strategy.ts
+++ nest-study/project/src/auth/jwt/jwt.strategy.ts
@@ -11,7 +11,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: 'secret',
+      secretOrKey: process.env.JWT_SECRET,
     });
   }
 
