5dcc4f2fb293dbb53722da6e57dd9cf9c4e11185
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-08-27 00:44:16 +0900

    Initialize project

---
+++ nest-study/slac/src/app.module.ts
@@ -0,0 +1,16 @@
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { LoggerMiddleware } from './common/middlewares/logger.middleware';
+
+@Module({
+  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
