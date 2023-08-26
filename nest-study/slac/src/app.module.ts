d2e316d543d92682b293b0176850ab24fd5d8f41
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-08-27 01:10:44 +0900

    Setting: Setting modules

--- nest-study/slac/src/app.module.ts
+++ nest-study/slac/src/app.module.ts
@@ -3,9 +3,19 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerMiddleware } from './common/middlewares/logger.middleware';
+import { UsersModule } from './users/users.module';
+import { WorkspacesModule } from './workspaces/workspaces.module';
+import { ChannelsModule } from './channels/channels.module';
+import { DmsModule } from './dms/dms.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    UsersModule,
+    WorkspacesModule,
+    ChannelsModule,
+    DmsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
